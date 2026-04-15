b09226d465bb51c015e0f84ba885780d8ff26769
Bruno Witt <brunowittn@gmail.com>
2026-04-15 08:47:16 -0300

    Delete ticketProject directory

--- ticketProject/src/Modules/Mensagem/DTOs/MessageDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace ticketProject.src.Modules.Mensagem.DTOs
-{
-    public class MessageDTO
-    {
-        public required int IdUsuario{get;set;}
-        public required int IdTicket{get;set;}
-        public required string Texto{get;set;}
-    }
-}
--- ticketProject/src/Modules/Usuario/Repository/IUsuarioRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace ticketProject.src.Modules.Usuario.Repository
-{
-    public interface IUsuarioRepository
-    {
-        Task<Models.Usuario?> GetByEmail(string emailInput);
-        Task CreateUser(string nome, string email, string senha, Models.PerfilUsuario perfil_usuario);
-    }
-}
