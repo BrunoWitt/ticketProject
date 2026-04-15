38c9b31920d6b02fdd100cea782f7e905370a011
BrunoWitt <brunowittn@gmail.com>
2026-04-15 10:35:46 -0300

    feat: BaseModel

---
+++ Src/Shared/Base/BaseModel.cs
@@ -0,0 +1,10 @@
+namespace Src.Shared.Base;
+
+public class BaseModel
+{
+    public int Id {get;set;}
+
+    public DateTimeOffset DataHoraCriado {get;set;}
+
+    public DateTimeOffset DataHoraDelecao {get;set;}
+}
