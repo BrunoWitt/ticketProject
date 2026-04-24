8662d567a78f117d29ecd9208118bc6d891ce3e6
BrunoWitt <brunowittn@gmail.com>
2026-04-24 15:41:25 -0300

    feat: adição do update

--- Src/Shared/Base/BaseRepository.cs
+++ Src/Shared/Base/BaseRepository.cs
@@ -107,7 +107,55 @@ public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity>
 
     public async Task UpdateAsync(TEntity entity)
     {
-        
+        var props = typeof(TEntity).GetProperties();
+
+        var idProp = props.FirstOrDefault(p => 
+            ToSnakeCase(p.Name) == IdColumn);
+
+        if (idProp == null)
+            throw new Exception($"Id property '{IdColumn}' not found in entity");
+
+        var setClause = string.Join(", ", props
+            .Where(p => ToSnakeCase(p.Name) != IdColumn)
+            .Select(p =>
+            {
+                var column = ToSnakeCase(p.Name);
+
+                if (p.PropertyType.IsEnum)
+                    return $"{column} = @{column}::perfil_usuario";
+
+                return $"{column} = @{column}";
+            }));
+
+        var sql = $"UPDATE {TableName} SET {setClause} WHERE {IdColumn} = @id";
+
+        using var conn = Connection;
+        await conn.OpenAsync();
+
+        using var cmd = new NpgsqlCommand(sql, conn);
+
+        foreach (var prop in props)
+        {
+            var column = ToSnakeCase(prop.Name);
+            var value = prop.GetValue(entity);
+
+            if (column == IdColumn)
+            {
+                cmd.Parameters.AddWithValue("id", value);
+                continue;
+            }
+
+            if (value is Enum)
+            {
+                cmd.Parameters.AddWithValue(column, value.ToString());
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue(column, value ?? DBNull.Value);
+            }
+        }
+
+        await cmd.ExecuteNonQueryAsync();
     }
 
 
